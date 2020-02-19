d3f0469d10824713cd64472765fa5a6f254c9cd2
pablorm296 <pablo.reyes.moctezuma@gmail.com>
2020-02-19 17:59:00 -0600

    LexppScrapper class updated

--- webScrapping/LexppScrapper/LexppScrapper.py
+++ webScrapping/LexppScrapper/LexppScrapper.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException, ElementNotVisibleException
 import logging
 
 #Definimos bitácora
@@ -20,8 +21,8 @@ class LexppScrapper:
         #Vamos a la url especificada por el usuario 
         self.webdriver.get(url)
         #Enviamos mensajes de depuración
-        logger.info("Nueva instancia de LexppScrapper iniciada en {0}".format(id(self)))
-        logger.info("Nuevo webdriver Firefox iniciado en {0}".format(id(self.webdriver)))
+        logger.info("New LexppScrapper instance created at {0}".format(id(self)))
+        logger.info("New Firefox driver created at {0}".format(id(self.webdriver)))
     
     #Reiniciar webdriver
     def restartWebDriver(self, headless = True, url = "https://www.google.com.mx"):
@@ -39,7 +40,7 @@ class LexppScrapper:
         #Vamos a la url especificada por el usuario 
         self.webdriver.get(url)
         #Enviamos mensajes de depuración
-        logger.info("Nuevo webdriver Firefox iniciado en {0}".format(id(self.webdriver)))
+        logger.info("New Firefox driver created at {0}".format(id(self.webdriver)))
 
     #Cerrar el webdriver
     def killWebDriver(self):
@@ -51,3 +52,84 @@ class LexppScrapper:
         #Verificamos parámetros
         if not isinstance(url, str):
             raise TypeError("'url' must be a string object")
+        #Vamos a la página
+        self.webdriver.get(url)
+    
+    #Clic en el objeto
+    def clickOn(self, element, findBy, continueOnExceptions = True):
+        #Verificamos parámetros
+        if not isinstance(element, str):
+            raise TypeError("'url' must be a string object")
+        if findBy not in ["name", "id", "xpath", "css selector", "class name"]:
+            raise ValueError("'findBy' must be name, id or xpath")
+        if not isinstance(continueOnExceptions, bool):
+            raise TypeError("'continueOnExceptions' must be a bool object")
+        #Buscamos elemento
+        try:
+            targetElem = self.__getSingleElement(element, findBy, continueOnExceptions)
+        except NoSuchElementException as e:
+            logger.warning("Exception while clicking: the specified element could not be found")
+            if not continueOnExceptions:
+                raise NoSuchElementException(e)
+        else:
+            #Le damos click al elemento
+            try:
+                targetElem.click()
+            except ElementNotVisibleException as e:
+                logger.warning("Exception while clicking: the specified element is not visible")
+                if not continueOnExceptions:
+                    raise ElementNotVisibleException(e)
+            except ElementClickInterceptedException as e:
+                logger.warning("Exception while clicking: the click action was intercepted")
+                if not continueOnExceptions:
+                    raise ElementClickInterceptedException(e)
+            except Exception as e:
+                logger.warning("Exception while clicking: unexpected error: {0}".format(e))
+                if not continueOnExceptions:
+                    raise
+    
+    #Obtener elemento
+    def getElement(self, element, findBy, multiple = False, continueOnExceptions = True):
+        #Verificamos parámetros
+        if not isinstance(element, str):
+            raise TypeError("'url' must be a string object")
+        if findBy not in ["name", "id", "xpath", "css selector", "class name"]:
+            raise ValueError("'findBy' must be name, id or xpath")
+        if not isinstance(multiple, bool):
+            raise TypeError("'multiple' must be a bool object")
+        if not isinstance(continueOnExceptions, bool):
+            raise TypeError("'continueOnExceptions' must be a bool object")
+        #El usuario quiere uno o varios elementos
+        if multiple:
+            targetElem = self.__getMultipleElements(element, findBy, continueOnExceptions)
+        else:
+            targetElem = self.__getSingleElement(element, findBy, continueOnExceptions)
+        
+        return targetElem
+
+    #Privativo: obtener varios elementos
+    def __getSingleElement(self, element, findBy, continueOnExceptions):
+        #Buscamos elemento
+        try:
+            targetElem = self.webdriver.find_element(findBy, element)
+        except NoSuchElementException as e:
+            logger.warning("Exception while locating element: the specified element could not be found")
+            if not continueOnExceptions:
+                raise NoSuchElementException(e)
+            else:
+                return None
+        else:
+            return targetElem
+    #Privativo: obtener un solo elementos
+    def __getMultipleElements(self, element, findBy, continueOnExceptions):
+        #Buscamos elemento
+        try:
+            targetElem = self.webdriver.find_elements(findBy, element)
+        except NoSuchElementException as e:
+            logger.warning("Exception while locating element: the specified element could not be found")
+            if not continueOnExceptions:
+                raise NoSuchElementException(e)
+            else:
+                return None
+        else:
+            return targetElem
