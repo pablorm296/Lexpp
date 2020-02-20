3cf036204bbd2f7bbee42b44c68ab0bb86f23f5c
pablorm296 <pablo.reyes.moctezuma@gmail.com>
2020-02-20 06:35:31 -0600

    Click on first tesis

--- webScrapping/tesis.py
+++ webScrapping/tesis.py
@@ -8,7 +8,7 @@ logger = logging.getLogger()
 #Definimos nivel de registro
 logger.setLevel(logging.INFO)
 #Definimos formato
-logFormatter = logging.Formatter("%(levelname)-8s|%(asctime)s|%(module)s: %(message)s")
+logFormatter = logging.Formatter("%(levelname)-8s|%(asctime)s|%(module)s|%(message)s", "%Y-%m-%d %H:%M:%S")
 #Definimos handlers para consola y archivo
 todayDate = datetime.date.today().strftime("%Y-%m-%d")
 logFileHandler = logging.FileHandler("./logs/tesis_{0}.log".format(todayDate))
@@ -35,6 +35,19 @@ myScrapper.webdriver.execute_script("CalcularEpocaLecturaSecuencial(0, 0);")
 
 #Esperamos
 myScrapper.waitUntil(".sec-resultados", "css selector")
+
+#Obtenemos lista de elementos
+itemList = myScrapper.getElement(".rubro a", "css selector", multiple = True, continueOnExceptions = False)
+
+#Seleccionamos el primer elemento
+firstElement = itemList[0]
+
+#Damos clic en el primer elemento
+#Al dar click accederemos al primer resultado de la búsqueda (una Tésis); entonces, podremos navegar a través de estos resultados
+firstElement.click()
+
+#Esperamos a que el texto de la tesis se carge
+myScrapper.sleep(5)
 
 #Matamos el webdriver
 myScrapper.killWebDriver()
