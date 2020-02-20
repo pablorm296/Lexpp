d33fd8556ce5976dc6ce9135d7f994d76cefc72e
pablorm296 <pablo.reyes.moctezuma@gmail.com>
2020-02-20 07:43:36 -0600

    main scrapping loop added

--- webScrapping/tesis.py
+++ webScrapping/tesis.py
@@ -1,6 +1,8 @@
 from LexppScrapper.LexppScrapper import LexppScrapper
 import logging
 import datetime
+import requests
+import re
 import sys
 
 #Definimos bitácora
@@ -8,7 +10,7 @@ logger = logging.getLogger()
 #Definimos nivel de registro
 logger.setLevel(logging.INFO)
 #Definimos formato
-logFormatter = logging.Formatter("%(levelname)-8s|%(asctime)s|%(module)s|%(message)s", "%Y-%m-%d %H:%M:%S")
+logFormatter = logging.Formatter("%(levelname)-8s|%(asctime)s|%(module)-10s|%(message)s", "%Y-%m-%d %H:%M:%S")
 #Definimos handlers para consola y archivo
 todayDate = datetime.date.today().strftime("%Y-%m-%d")
 logFileHandler = logging.FileHandler("./logs/tesis_{0}.log".format(todayDate))
@@ -47,7 +49,50 @@ firstElement = itemList[0]
 firstElement.click()
 
 #Esperamos a que el texto de la tesis se carge
-myScrapper.sleep(5)
+myScrapper.sleep(1)
+
+#Obtenemos número de tesis y total
+tesisCounterElement = myScrapper.getElement("lblNavegacion", "id", continueOnExceptions = False)
+tesisCounterText = tesisCounterElement.text
+tesisCounterMatch = re.findall(r"(\d*)\sde\s(\d*)", tesisCounterText)
+current = int(tesisCounterMatch[0][0])
+total = int(tesisCounterMatch[0][1])
+
+while current < total:
+    
+    #Si es la primera página, nos saltamos obtener el número de página
+    if current > 1:
+        #Obtenemos número de tesis y total
+        tesisCounterElement = myScrapper.getElement("lblNavegacion", "id", continueOnExceptions = False)
+        tesisCounterText = tesisCounterElement.text
+        tesisCounterMatch = re.findall(r"(\d*)\sde\s(\d*)", tesisCounterText)
+        current = tesisCounterMatch[0][0]
+        total = tesisCounterMatch[0][1]
+
+    #Obtenemos datos necesarios para hacer la request
+    reqParameters = dict()
+    reqParameters.update(Url = myScrapper.webdriver.execute_script("return window.location.href;"))
+    reqParameters.update(IdSesion = myScrapper.webdriver.execute_script("return IdSession;"))
+    reqParameters.update(IdActual = myScrapper.webdriver.execute_script("return $(ObtenerControlesServidor().idActual).val();"))
+    reqParameters.update(IdELementos = "")
+    reqParameters.update(NumeroElementos = "20")
+    reqParameters.update(Pagina = "0")
+    reqParameters.update(tesisDesmarcadas = "")
+    reqParameters.update(Desmarcar = 0)
+    logger.info("Parámetros de solicitud: {0}".format(reqParameters))
+
+    #Mandamos la request
+    tesisDataRespose = requests.post("https://sjf.scjn.gob.mx/sjfsist/Servicios/wsTesis.asmx/ObtenerDetalle", json = reqParameters)
+    tesisDataJson = tesisDataRespose.json()
+    tesisDataJson = tesisDataJson.get("d")
+    print(tesisDataJson)
+
+    #Damos clic para avanzar a la siguiente página
+    myScrapper.clickOn("imgSiguiente", "id", continueOnExceptions = False)
+
+    #Esperamos a que el texto de la tesis se carge
+    myScrapper.sleep(2)
+
 
 #Matamos el webdriver
 myScrapper.killWebDriver()
