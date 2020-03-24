ecc61e9060f8d2ed0ef043890955732424bc8dfd
pablorm296 <pablo.reyes.moctezuma@gmail.com>
2020-03-24 15:34:57 -0600

    Shell script to import downloaded docs to MongoDb

---
+++ webScrapping/addDocsToMongo.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#===============================================================================
+# FILE: addDocsToMongo.sh
+# DESCRIPTION: Agrega documentos obtenidos por los scripts de webScrapping a la base
+#               de datos en MongoDB.
+# AUTHOR: Pablo Reyes
+#===============================================================================
+
+# Some debug info
+echo "Working directory: $0"
+echo "Target directory: $1"
+
+# Rename parameter
+targetDir=$1
+
+# Get last character of target dir
+lastChar="${targetDir: -1}"
+
+# If last char equals "/" then leave targetDir as is, else, append slash
+if ["$lastChar" != "/"]; then
+    targetDir=$targetDir"/"
+fi
+
+# Append registered folder
+registeredDir=$targetDir"registered/"
+
+# Create folder
+mkdir -p "$targetDir"
+
+# Define a function that process each file
+importDoc() {
+    echo "$1"
+}
+
+# Export function
+export -f importDoc
+
+#Parallel
+parallel importDoc ::: "$targetDir"
