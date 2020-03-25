136405e9f1a82b91714ac3075409081a0c24830e
pablorm296 <pablo.reyes.moctezuma@gmail.com>
2020-03-24 21:39:39 -0600

    fixed wrong format order

--- webScrapping/addDocContent.py
+++ webScrapping/addDocContent.py
@@ -49,7 +49,7 @@ def main(arguments):
     docExtension = targetFile.split("_")[1].split(".")[1]
 
     # Log info
-    logging.debug("ID: {0} | Type: {2} | Extension {3}".format(LexppId_Library, docType, docExtension))
+    logging.debug("ID: {0} | Type: {1} | Extension {2}".format(LexppId_Library, docType, docExtension))
 
 
 if __name__ == "__main__":
